9ccb96bdbe87ea35aa10896937c4bd0521c2b470
Kody Stremick <kody.stremick@gmail.com>
2023-05-14 23:01:49 +0100

    changed

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "golang.org/x/exp/constraints"
+
 // Contains checks if a slice contains an item
 func Contains[T comparable](list []T, item T) bool {
 	for _, v := range list {
@@ -20,4 +22,17 @@ func Remove[T comparable](list []T, item T) []T {
 	}
 
 	return list
+}
+
+// ArgMax computes the index of the max value
+func ArgMax[T constraints.Ordered](list []T) int {
+	var index int
+	var max T
+
+	for i, val := range list {
+		if val > max {
+			index = i
+		}
+	}
+	return index
 }
